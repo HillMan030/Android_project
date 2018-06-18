1101e323b91d5e725161b55f1f17ecb16705b3d9
d0426511 <wchill23@gmail.com>
2018-06-18 17:38:49 +0800

    android

--- app/src/main/java/com/example/zeno/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zeno/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//123456
+//1234567
         ListView listview = (ListView) findViewById(R.id.listview);
         Button btn = (Button)findViewById(R.id.btn) ;
 
