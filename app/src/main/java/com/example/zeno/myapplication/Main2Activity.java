13e71ed12e87779400d5629952f773fe2ac087ad
DESKTOP-PT755KG\Kai <jackyarta@gmail.com>
2018-06-19 16:01:21 +0800

    image added

--- app/src/main/java/com/example/zeno/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/zeno/myapplication/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.zeno.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
@@ -11,22 +12,56 @@ public class Main2Activity extends AppCompatActivity {
     String[] dec = {"亞洲料理", "", "日式料理", "", "", "亞洲料理, 韓式料理", "亞洲料理, 台灣小吃/台菜", "", "亞洲料理, 台灣小吃/台菜", "", "亞洲料理, 韓式料理", "", "日式料理, 燒烤", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
     String[] loc = {"台灣台中西屯區文華路127巷6號", "台灣台中西屯區文華路6號", "台灣台中西屯區文華路121-3號", "台灣台中西屯區逢甲路20巷26-3號407", "台灣台中西屯區文華路99-2號", "台灣台中西屯區西平里文華路127巷19號", "台灣台中西屯區逢甲路20巷42號", "台灣台中西屯區逢甲路5號", "台'灣台中西屯區逢甲路74號407", "台灣台中西屯區逢甲路73號", "台灣台中西屯區逢甲路88號", "台灣台中西屯區逢甲路20巷47號", "台灣台中西屯區文華路121-29號407", "台灣台中台中市西屯區河南路二段256號", "台灣台中西屯區文華路150巷18號407", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
     String[] phone = {"+886 4 2451 9949", "+886 4 2451 9666", "", "+886 4 2452 6078", "+886 989 441 303", "+886 988 328 178", "+886 4 2451 4043", "+886 921 331 245", "+886 4 2451 3688", "+886 936 965 963", "+886 4 2451 7567", "+886 4 2451 9684", "+886 4 2451 1923", "", "+886 913 146 878", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
-
+    private int[] image = {
+            R.drawable.i3,
+            R.drawable.i4,
+            R.drawable.i6,
+            R.drawable.i7,
+            R.drawable.i12,
+            R.drawable.i13,
+            R.drawable.i15
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-//helloworld1
         Intent intent = getIntent();
         int position = intent.getIntExtra("position", 0);
-
         TextView result = (TextView) findViewById(R.id.result);
         TextView descreiption = (TextView) findViewById(R.id.descreiption);
         TextView location = (TextView) findViewById(R.id.location);
         TextView phonenum = (TextView) findViewById(R.id.phonenum);
+        ImageView image =(ImageView)findViewById(R.id.image);
         result.setText(str1[position]);
-           descreiption.setText(dec[position]);
+        descreiption.setText(dec[position]);
         location.setText(loc[position]);
         phonenum.setText(phone[position]);
+        //image
+        switch(position){
+            case 2:
+                image.setImageResource(R.drawable.i3);
+                break;
+            case 3:
+                image.setImageResource(R.drawable.i4);
+                break;
+            case 5:
+                image.setImageResource(R.drawable.i6);
+                break;
+            case 6:
+                image.setImageResource(R.drawable.i7);
+                break;
+            case 11:
+                image.setImageResource(R.drawable.i12);
+                break;
+            case 12:
+                image.setImageResource(R.drawable.i13);
+                break;
+            case 14:
+                image.setImageResource(R.drawable.i15);
+                break;
+            default:
+                image.setImageResource(R.drawable.i00);
+                break;
+        }
     }
 }
